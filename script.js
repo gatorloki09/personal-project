fac424488d43ac400a38d9d5ed3cc2c5407b3a45
gatorloki09 <sarsembaevtair4@gmail.com>
2025-08-09 17:34:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const todoValue = document.getElementById("todoText"),
-listItems = document.getElementById("list-items"),
-addUpdateClick = document.getElementById(""),
+  listItems = document.getElementById("list-items"),
+  addUpdateClick = document.getElementById("AddUpdateClick");
+
+  function CreateToDoData() {
+    console.log(todoValue.value);
+    if (todoValue.value){
+      
+    }
+  } 

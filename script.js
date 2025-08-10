debd2829ea00d5812fbe3090dfd05ce5f587153a
gatorloki09 <sarsembaevtair4@gmail.com>
2025-08-10 19:18:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ const todoValue = document.getElementById("todoText"),
   listItems = document.getElementById("list-items"),
   addUpdateClick = document.getElementById("AddUpdateClick");
 
-  function CreateToDoData() {
-    console.log(todoValue.value);
-    if (todoValue.value){
-      
-    }
-  } 
+todoValue.addEventListener("keypress", function(e){
+  if(e.key === "Enter"){
+    addUpdateClick.click();
+  }
+})
+function CreateToDoData() {
+    
+  if (todoValue.value === ""){
+    alert("Please Enter Your Reminder);
+    todoValue.focus();
+  }
+  let li = document.createElement("li");
+  const todoItems = '<div{todoValue.value}</div><div><img class = "edit" src ="/image/pencil.png"><img class = "delete" src ="/image/delete.png"/></div>';
+
+  li.innerHTML=todoItems;
+  listItems.appendChild(li);
+  todoValue.value = "";
+} 

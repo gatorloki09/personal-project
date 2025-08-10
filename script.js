2e9eace7a5095bde76c6e7d137da875b9cae680c
gatorloki09 <sarsembaevtair4@gmail.com>
2025-08-10 19:38:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,31 @@ function CreateToDoData() {
   });
 }
 
+
+
+
+
+
+const todoValue = document.getElementById("todoText"),
+  listItems = document.getElementById("list-items"),
+  addUpdateClick = document.getElementById("AddUpdateClick");
+
+todoValue.addEventListener("keypress", function(e){
+  if(e.key === "Enter"){
+    addUpdateClick.click();
+  }
+})
+function CreateToDoData() {
+    
+  if (todoValue.value === ""){
+    alert("Please Enter Your Reminder);
+    todoValue.focus();
+  }
+  let li = document.createElement("li");
+  const todoItems = '<div{todoValue.value}</div><div><img class = "edit todo-controls" src ="/image/pencil.png"><img class = "delete todo-controls" src ="/image/delete.png"/></div>';
+
+  li.innerHTML=todoItems;
+  listItems.appendChild(li);
+  todoValue.value = "";
+} 
+

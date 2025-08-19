47c49b0efde3800dddcec13e56f4bf2bf08a1c7b
gatorloki09 <sarsembaevtair4@gmail.com>
2025-08-19 12:05:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,42 +2,22 @@ const todoValue = document.getElementById("todoText"),
   listItems = document.getElementById("list-items"),
   addUpdateClick = document.getElementById("AddUpdateClick");
 
-// Allow pressing "Enter" to add
-todoValue.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
+todoValue.addEventListener("keypress", function(e){
+  if(e.key === "Enter"){
     addUpdateClick.click();
   }
-});
-
+})
 function CreateToDoData() {
-  if (todoValue.value.trim() === "") {
-    alert("Please Enter Your Reminder");
+    
+  if (todoValue.value === ""){
+    alert("Please Enter Your Reminder);
     todoValue.focus();
-    return; // stop here
   }
-
   let li = document.createElement("li");
-  const todoItems = `
-    <div>${todoValue.value}</div>
-    <div>
-      <img class="edit todo-controls" src="images/pencil.png" alt="Edit">
-      <img class="delete todo-controls" src="images/delete.png" alt="Delete">
-    </div>
-  `;
+  const todoItems = '<div{todoValue.value}</div><div><img class = "edit todo-controls" src ="/image/pencil.png"><img class = "delete todo-controls" src ="/image/delete.png"/></div>';
 
-  li.innerHTML = todoItems;
+  li.innerHTML=todoItems;
   listItems.appendChild(li);
   todoValue.value = "";
-
-  // Add delete functionality
-  li.querySelector(".delete").addEventListener("click", function () {
-    li.remove();
-  });
-
-  // Add edit functionality
-  li.querySelector(".edit").addEventListener("click", function () {
-    todoValue.value = li.firstElementChild.textContent; // put text back in input
-    li.remove();
-  });
-}
+} 
 

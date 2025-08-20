13cd8b25b4d19030d24b9cdd96d3e5600fc0b0e3
gatorloki09 <sarsembaevtair4@gmail.com>
2025-08-20 14:03:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const todoValue = document.getElementById("todoText"),
   listItems = document.getElementById("list-items"),
   addUpdateClick = document.getElementById("AddUpdateClick");
 
-let editTodo = null; // track if editing a task
-
-// Allow pressing Enter to add task
-todoValue.addEventListener("keypress", function(e){
-  if(e.key === "Enter"){
+// Allow pressing "Enter" to add
+todoValue.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
     addUpdateClick.click();
   }
 });
@@ -15,39 +13,30 @@ function CreateToDoData() {
   if (todoValue.value.trim() === "") {
     alert("Please Enter Your Reminder");
     todoValue.focus();
-    return;
+    return; // stop here
   }
 
-  if (editTodo) {
-    // If editing, just update text
-    editTodo.firstChild.textContent = todoValue.value;
-    editTodo = null;
-    addUpdateClick.src = "add-icon-design-in-round-shape-png-1.png";
-  } else {
-    // Create new task
-    let li = document.createElement("li");
-    li.innerHTML = `
-      <span>${todoValue.value}</span>
-      <div>
-        <img class="edit todo-controls" src="image/pencil.png" onclick="editTask(this)" />
-        <img class="delete todo-controls" src="image/delete.png" onclick="deleteTask(this)" />
-      </div>
-    `;
-    listItems.appendChild(li);
-  }
+  let li = document.createElement("li");
+  const todoItems = `
+    <div>${todoValue.value}</div>
+    <div>
+      <img class="edit todo-controls" src="images/pencil.png" alt="Edit">
+      <img class="delete todo-controls" src="images/delete.png" alt="Delete">
+    </div>
+  `;
 
+  li.innerHTML = todoItems;
+  listItems.appendChild(li);
   todoValue.value = "";
-}
 
-// Delete task
-function deleteTask(e) {
-  e.parentElement.parentElement.remove();
-}
+  // Add delete functionality
+  li.querySelector(".delete").addEventListener("click", function () {
+    li.remove();
+  });
 
-// Edit task
-function editTask(e) {
-  editTodo = e.parentElement.parentElement;
-  todoValue.value = editTodo.firstChild.textContent;
-  addUpdateClick.src = "image/update.png"; // change button icon
-  todoValue.focus();
+  // Add edit functionality
+  li.querySelector(".edit").addEventListener("click", function () {
+    todoValue.value = li.firstElementChild.textContent; // put text back in input
+    li.remove();
+  });
 }
